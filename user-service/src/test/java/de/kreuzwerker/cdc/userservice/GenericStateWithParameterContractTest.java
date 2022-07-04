62397bbb6986bba50ed311b7ff9abeb34d7deb91
Yaya Diallo, Mamadou <mamadou.yayadiallo@lacapitale.com>
2022-07-04 08:41:39 -0400

    update

--- user-service/src/test/java/de/kreuzwerker/cdc/userservice/GenericStateWithParameterContractTest.java
+++ user-service/src/test/java/de/kreuzwerker/cdc/userservice/GenericStateWithParameterContractTest.java
@@ -21,7 +21,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 @Provider("user-service")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //pact_broker is the service name in docker-compose
-@PactBroker(host = "pact_broker", tags = "${pactbroker.tags:prod}")
+@PactBroker(host = "localhost", port = "8005", tags = "junit5")
 public class GenericStateWithParameterContractTest {
 
     @TestTarget
